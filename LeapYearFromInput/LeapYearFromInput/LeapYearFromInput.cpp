4e6974d9860ee5c5f31f5be394337dde8644ea24
tamellLUU <trevor.amell@activision.com>
2022-07-26 14:32:20 -0400

    Modified LeapYearFromInput Challenge
    - Comment Cleanup
    - Extracted Math Functions
    - Added Input Validation

--- LeapYearFromInput/LeapYearFromInput/LeapYearFromInput.cpp
+++ LeapYearFromInput/LeapYearFromInput/LeapYearFromInput.cpp
@@ -2,16 +2,60 @@
 
 using namespace std;
 
+bool CenturyFactorOf400(int year);
+bool YearFactorOf4(int year);
+void LeapYearCheck(int year);
+
+void InvalidInputEvent();
+
 int main()
 {
-    cout << "Please Input a year to test if it's a leap year: ";
-    unsigned int inputYear;
-    cin >> inputYear;
+    while (true)
+    {
+        cout << "Please input a year to test if it's a leap year: ";
+        unsigned int inputYear;
+        cin >> inputYear;
 
-    if (inputYear % 4 != 0 || (inputYear % 100 == 0 && inputYear % 400 != 0)){
-        cout << inputYear << " is not a leap year!" << endl;
+        if (cin.fail()) {
+            InvalidInputEvent();
+            continue;
+        }
+
+        LeapYearCheck(inputYear);
+        break;
     }
-    else {
-        cout << inputYear << " is a leap year!" << endl;
+}
+
+void LeapYearCheck(int year)
+{
+    //https://www.mathsisfun.com/leap-years.html
+    if (YearFactorOf4(year) && CenturyFactorOf400(year))
+    {
+        cout << year << " is a leap year!" << endl;
     }
+    else
+    {
+        cout << year << " is not a leap year!" << endl;
+    }
+}
+
+bool YearFactorOf4(int year)
+{
+    if (year % 4 != 0) return false;
+    return true;
+}
+
+bool CenturyFactorOf400(int year) 
+{
+    if (year % 100 == 0 && year % 400 != 0) return false;
+    return true;
+}
+
+// Clear cin and warn user
+void InvalidInputEvent() 
+{
+    cin.clear();
+    cin.ignore(99999, '\n');
+    system("cls");
+    cout << "Invalid Input!" << endl;
 }
