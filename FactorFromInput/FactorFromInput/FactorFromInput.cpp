573a6bad64c0c87c9caf3bff6b095b3c2a9196b2
tamellLUU <trevor.amell@activision.com>
2022-07-26 16:21:42 -0400

    Modified FactorFromInput Challenge
    - Cleaner Loop Syntax
    - Input Validation
    - Factor logic Extracted to Functions

--- FactorFromInput/FactorFromInput/FactorFromInput.cpp
+++ FactorFromInput/FactorFromInput/FactorFromInput.cpp
@@ -2,40 +2,81 @@
 
 using namespace std;
 
+void InvalidInputEvent();
+
+int GetFactorAmount(int val);
+bool isFactor(int val, int possFactor);
+
+void PopulateArray(int* arrayToPopulate, int val);
+void OutputArray(int* factorArray, int arraySize);
+
 int main()
 {
-	// Loop Input until valid
-	while (true) 
+	int userVal, arraySize;
+	int* factorArray;
+
+	while (true)
 	{
-		int userVal;
 		cout << "Enter a number to find its factors!: ";
 		cin >> userVal;
 
-		//If Cin fail, clear and ignore all input and try again
-		if (cin.fail()) 
-		{
-			cin.clear();
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			cout << "Invalid input!" << endl;
-		} 
-		else 
-		{
-			// Loop through numbers until reaching sqrt
-			// Floor to avoid duplicates
-			for (int i = 1; i <= floor(sqrt(userVal)); i++) {
+		if (!cin.fail()) break;
+		InvalidInputEvent();
+	}
 
-				// If % == 0, showcase the number tested, and the number of times it goes into the value
-				
-				if (userVal % i == 0) 
-				{ 
-					// Print in pairs
-					cout << i << " " << (userVal / i) << " " << endl;
-				}
-			}
+	// Get Array size prior to initialization, warrants running through another loop
+	arraySize = GetFactorAmount(userVal);
+	factorArray = new int[arraySize];
+	PopulateArray(factorArray, userVal);
+	OutputArray(factorArray, arraySize);
 
-			// Break out of input loop once fully achieved success
-			break;
+	// Cleanup
+	delete[] factorArray;
+	factorArray = nullptr;
+}
+
+void OutputArray(int* factorArray, int arraySize)
+{
+	for (int i = 0; i < arraySize; i++)
+	{
+		cout << factorArray[i] << " ";
+	}
+}
+
+void PopulateArray(int* arrayToPopulate, int val)
+{
+	int position = 0;
+	for (int i = 1; i <= val; i++)
+	{
+		// Input Element in Array if Factor at position
+		if (isFactor(val, i))
+		{
+			*(arrayToPopulate + position) = i;
+			position++;
 		}
 	}
-	
+}
+
+// Get Amount of factors for val for array initialization
+int GetFactorAmount(int val) 
+{
+	int counter = 0;
+	for (int i = 1; i <= val; i++)
+	{
+		if (isFactor(val, i)) counter++;
+	}
+	return counter;
+}
+
+bool isFactor(int val, int possFactor)
+{
+	return (val % possFactor == 0);
+}
+
+void InvalidInputEvent()
+{
+	cin.clear();
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	system("cls");
+	cout << "Invalid input!" << endl;
 }
